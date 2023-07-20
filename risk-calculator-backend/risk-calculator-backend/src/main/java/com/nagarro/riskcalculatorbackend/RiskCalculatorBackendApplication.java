9ce7d5b2ca8cdbc160a8ed9a81c4555aeb0b36ff
pgNagarro <paras.gautam@nagarro.com>
2023-07-20 11:48:35 +0530

    Initial

---
+++ risk-calculator-backend/risk-calculator-backend/src/main/java/com/nagarro/riskcalculatorbackend/RiskCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.riskcalculatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ risk-calculator-backend/risk-calculator-backend/src/test/java/com/nagarro/riskcalculatorbackend/RiskCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.riskcalculatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
